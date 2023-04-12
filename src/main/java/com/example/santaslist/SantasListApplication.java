ba5de2d42e8cfb8c8618cd8e61868dbec7090204
Nicolai Ene <35382666+SkyHighSpirits@users.noreply.github.com>
2023-04-12 09:03:09 +0200

    Initial commit

---
+++ src/main/java/com/example/santaslist/SantasListApplication.java
@@ -0,0 +1,13 @@
+package com.example.santaslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantasListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantasListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/santaslist/SantasListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.santaslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantasListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
