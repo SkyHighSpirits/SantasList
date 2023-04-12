d007a7a5280f5797f7724c586b4f958df847ead7
Nicolai Ene <35382666+SkyHighSpirits@users.noreply.github.com>
2023-04-12 10:44:23 +0200

    SQL createDB.sql script

---
+++ src/main/resources/mysql/createDB.sql
@@ -0,0 +1,26 @@
+DROP database IF exists santalistdb;
+CREATE DATABASE santalistdb;
+USE santalistdb;
+
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS wishes;
+
+CREATE TABLE users (
+	userID INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,	
+	email VARCHAR(255) UNIQUE NOT NULL,
+    firstName VARCHAR(255) NOT NULL,
+    lastName VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE wishes (
+	wishID INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	userID INT NOT NULL,
+    wishName VARCHAR(40) NOT NULL,
+    price FLOAT(1000000, 2) NOT NULL,
+    priority INT,
+    wishDescription VARCHAR(512),
+    url VARCHAR(512),
+    reserved BOOL,
+    FOREIGN KEY(userID) REFERENCES users(userID)
+);
+
