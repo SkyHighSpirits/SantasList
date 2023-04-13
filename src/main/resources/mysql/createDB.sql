c544f774cf7f241dc8bcac440da8a67e55d88e6f
Nicolai Ene <35382666+SkyHighSpirits@users.noreply.github.com>
2023-04-13 08:43:20 +0200

    added password field in createDB.sql

--- src/main/resources/mysql/createDB.sql
+++ src/main/resources/mysql/createDB.sql
@@ -6,21 +6,22 @@ DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS wishes;
 
 CREATE TABLE users (
-     userID INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
-     email VARCHAR(255) UNIQUE NOT NULL,
-     firstName VARCHAR(255) NOT NULL,
-     lastName VARCHAR(255) NOT NULL
+                       userID INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                       email VARCHAR(255) UNIQUE NOT NULL,
+                       userPassword varchar(255) NOT NULL,
+                       firstName VARCHAR(255) NOT NULL,
+                       lastName VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE wishes (
-    wishID INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    userID INT NOT NULL,
-    wishName VARCHAR(40) NOT NULL,
-    price FLOAT(255, 2) NOT NULL,
-    priority INT,
-    wishDescription VARCHAR(512),
-    url VARCHAR(512),
-    reserved BOOL,
-    FOREIGN KEY(userID) REFERENCES users(userID)
+                        wishID INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                        userID INT NOT NULL,
+                        wishName VARCHAR(40) NOT NULL,
+                        price FLOAT(255, 2) NOT NULL,
+                        priority INT,
+                        wishDescription VARCHAR(512),
+                        url VARCHAR(512),
+                        reserved BOOL,
+                        FOREIGN KEY(userID) REFERENCES users(userID)
 );
 
