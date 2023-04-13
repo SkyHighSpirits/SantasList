f75acae59068bae243fd53151758df48bf674ce9
Nicolai Ene <35382666+SkyHighSpirits@users.noreply.github.com>
2023-04-13 11:26:53 +0200

    Implemented models for User.java and Wish.java with getters setters, to string and constructors

--- src/main/java/com/example/santaslist/model/User.java
+++ src/main/java/com/example/santaslist/model/User.java
@@ -1,4 +1,78 @@
 package com.example.santaslist.model;
 
 public class User {
+
+    private int userID;
+    private String email;
+    private String password;
+
+    private String firstName;
+
+    private String lastName;
+
+    public User()
+    {
+
+    }
+    public User(int userID, String email, String password, String firstName, String lastName)
+    {
+        this.userID = userID;
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userID=" + userID +
+                ", email='" + email + '\'' +
+                ", userPassword=" + "******" +
+                ", firstName=" + firstName +
+                ", lastName=" + lastName +
+                '}';
+    }
+
 }
--- src/main/java/com/example/santaslist/model/Wish.java
+++ src/main/java/com/example/santaslist/model/Wish.java
@@ -1,4 +1,107 @@
 package com.example.santaslist.model;
 
 public class Wish {
+
+    private int wishID;
+    private int userID;
+    private String wishName;
+    private float price;
+    private int priority;
+    private String wishDescription;
+    private String url;
+    private boolean reserved;
+
+    public Wish()
+    {
+
+    }
+    public Wish(int wishID, int userID, String wishName, float price, int priority, String wishDescription, String url, boolean reserved) {
+        this.wishID = wishID;
+        this.userID = userID;
+        this.wishName = wishName;
+        this.price = price;
+        this.priority = priority;
+        this.wishDescription = wishDescription;
+        this.url = url;
+        this.reserved = reserved;
+    }
+
+    public int getWishID() {
+        return wishID;
+    }
+
+    public void setWishID(int wishID) {
+        this.wishID = wishID;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getWishName() {
+        return wishName;
+    }
+
+    public void setWishName(String wishName) {
+        this.wishName = wishName;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public String getWishDescription() {
+        return wishDescription;
+    }
+
+    public void setWishDescription(String wishDescription) {
+        this.wishDescription = wishDescription;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public boolean isReserved() {
+        return reserved;
+    }
+
+    public void setReserved(boolean reserved) {
+        this.reserved = reserved;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Wish{" +
+                "wishID=" + wishID +
+                ", userID=" + userID +
+                ", wishName='" + wishName + '\'' +
+                ", price=" + price +
+                ", priority=" + priority +
+                ", wishDescription='" + wishDescription + '\'' +
+                ", url='" + url + '\'' +
+                ", reserved=" + reserved +
+                '}';
+    }
 }
