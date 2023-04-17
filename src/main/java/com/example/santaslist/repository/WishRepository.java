a2b57f19bc1cfa5a74d93e7f046c32c0fea78169
Nicolai Ene <35382666+SkyHighSpirits@users.noreply.github.com>
2023-04-17 11:21:08 +0200

    Added getAllForUser(int id) method in WishRepository.java

--- src/main/java/com/example/santaslist/repository/WishRepository.java
+++ src/main/java/com/example/santaslist/repository/WishRepository.java
@@ -20,6 +20,36 @@ public class WishRepository
     @Value("${spring.datasource.password}")
     private String PWD;// = "#Danmark2300";
 
+
+    public List<Wish> getAllForUser(int Id){
+        List<Wish> wishes = new ArrayList<>();
+        try {
+            Connection connection = DriverManager.getConnection(DB_URL, UID, PWD);
+            Statement statement = connection.createStatement();
+            final String SQL_QUERY = "SELECT * FROM wishes WHERE userID = Id";
+            ResultSet resultSet = statement.executeQuery(SQL_QUERY);
+            while (resultSet.next()){
+                int wishID = resultSet.getInt(1);
+                int userID = resultSet.getInt(2);
+                String wishName = resultSet.getString(3);
+                int price = resultSet.getInt(4);
+                int priority = resultSet.getInt(5);
+                String wishDescription = resultSet.getString(6);
+                String url = resultSet.getString(7);
+                boolean reserved = resultSet.getBoolean(8);
+                Wish wish = new Wish(wishID, userID, wishName, price, priority, wishDescription, url, reserved);
+                wishes.add(wish);
+                System.out.println(wish);
+            }
+        }
+        catch(SQLException e)
+        {
+            System.out.println("Could not query database");
+            e.printStackTrace();
+        }
+        return wishes;
+    }
+
     public List<Wish> getAll(){
         List<Wish> wishes = new ArrayList<>();
         try {
